c0a30cf39135e93cd6374b072ba7e444e9840405
LEEDOX <leedox@naver.com>
2023-10-26 18:50:40 +0900

    타이틀

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,9 +4,8 @@ const Title = ({ username }) => {
   return (
     <div className="container my-2">
       <div className="row border-bottom">
-        <div className="col-6"></div>
-        <div className="col-6 text-end">
-          <span>{username}님 환영합니다!</span>
+        <div className="text-end">
+          <span>[{username}]님 환영합니다!</span>
         </div>
       </div>
     </div>
